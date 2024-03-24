63b97c4382dc7e795deb0f1844354878c06de18d
BC-driver <BC_driver@163.com>
2024-03-24 20:47:58 +0800

    Create Env, Dijkstra

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,1 +1,26 @@
 import heapq
+import Envrionment
+
+class node:
+    def __init__(self):
+        self.next = 4
+        self.inf = 1e10
+        self.dis = self.inf
+
+class Dijkstra:
+    def __init__(self, start, end, env):
+        self.env = env
+        self.start = start
+        self.end = end
+        self.height = self.env.getSize()[0]
+        self.width = self.env.getSize()[1]
+        self.graph = [[node() for i in range(self.width)] for i in range(self.height)]
+
+
+def main():
+    mymap = Envrionment.Envrionment(10, 10)
+    mymap.showenv()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Envrionment.py
@@ -0,0 +1,31 @@
+class Envrionment:
+    def __init__(self, width, height):
+
+        self.width = width
+        self.height = height
+        self.env = [[0 for i in range(width)] for i in range(height)]
+
+        self.setObs(0, 4, 1, 4)
+        self.setObs(3, 0, 4, 4)
+        self.setObs(2, 6, 5, 7)
+        self.setObs(7, 2, 7, 3)
+        self.setObs(6, 4, 7, 5)
+        self.setObs(7, 7, 8, 8)
+
+
+    def getEnv(self, x, y):
+        return self.env[x][y]
+
+    def getSize(self):
+        return (self.height, self.height)
+
+    def showenv(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print(".#PBSE"[self.env[i][j]], end='')
+            print()
+
+    def setObs(self, ltx, lty, rbx, rby):
+        for i in range(ltx, rbx + 1):
+            for j in range(lty, rby + 1):
+                self.env[i][j] = 1;
