603c7d5a064c2901fa6eab980ca6fd899c5adbdb
BC-driver <BC_driver@163.com>
2024-07-06 21:07:32 +0800

    Imporved Environment class

--- Envrionment.py
+++ Envrionment.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class Envrionment:
     def __init__(self, width, height):
 
@@ -45,3 +47,57 @@ def envOne():
     env.setObs(7, 7, 8, 8)
     env.setObs(6, 6, 6, 6)
     return env
+
+class ContiEnvironment():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.obstacleRect = []
+
+    def showPath(self, path):
+        cur = path[0]
+        for point in path[1:]:
+            plt.plot([cur[0], point[0]], [cur[1], point[1]], color="green")
+            cur = point
+        plt.show()
+
+    def getHeight(self):
+        return self.height
+
+    def getWidth(self):
+        return self.width
+
+    def addRect(self, left_bottom, right_top):
+        self.obstacleRect.append((left_bottom, right_top))
+
+    def outProduct(self, a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    def isNoLineCollision(self, a, b, c, d):
+        m = self.outProduct(a, c)
+        n = self.outProduct(a, d)
+        if m == 0 and n == 0:
+            if max(a[0], b[0]) < min(c[0], d[0]) or \
+                max(a[1], b[1]) < min(c[1], d[1]) or \
+                min(a[0], b[0]) < max(c[0], d[0]) or \
+                min(a[1], b[1]) < max(c[1], d[1]):
+                return True
+            return False
+        return m * n > 0
+
+    def isNoCollision(self, u, v):
+        flag = True
+        for rect in self.obstacleRect:
+            flag &= self.isNoLineCollision(u, v, rect[0], (rect[0][0], rect[1][1]))
+            flag &= self.isNoLineCollision(u, v, (rect[0][0], rect[1][1]), rect[1])
+            flag &= self.isNoLineCollision(u, v, (rect[1][0], rect[0][1]), rect[1])
+            flag &= self.isNoLineCollision(u, v, rect[0], (rect[1][0], rect[0][1]))
+            if flag == False:
+                break
+        return flag
+
+
+if __name__ == "__main__":
+    env = ContiEnvironment(100, 100)
+    env.addRect((1, 1), (5, 5))
+    print(env.isNoCollision((0, 1), (6, 1)))
---
+++ rrt.py
@@ -0,0 +1,90 @@
+from Envrionment import ContiEnvironment
+import random as R
+
+class Node():
+    def __init__(self, pos):
+        self.pos = pos
+        self.pre = pos
+
+    def setPre(self, pre):
+        self.pre = pre
+class RRT():
+    def __init__(self, env, start, end):
+        self.env = env
+        self.start = Node(start)
+        self.end = Node(end)
+        self.T = []
+
+
+    def getRandomPoint(self):
+        x = self.env.getWidth() * R.random()
+        y = self.env.getHeight() * R.random()
+        return Node((x, y))
+
+    def disCal(self, a, b):
+        return ((a.pos[0] - b.pos[0]) ** 2 + (a.pos[1] - b.pos[1]) ** 2) ** 0.5
+
+    def isNoCollision(self, a, b):
+        return True
+
+    def getNearestPoint(self, xrand):
+        mincost = self.env.getWidth() ** 2 + self.env.getHeight() ** 2
+        minnode = self.T[0]
+        minidx = -1
+        #print(xrand.pos)
+        for i in range(len(self.T)):
+            node = self.T[i]
+            if self.disCal(xrand, node) < mincost:
+                #print(mincost, minnode.pos, end="")
+                mincost = self.disCal(xrand, node)
+                minnode = node
+                minidx = i
+                #print(mincost, minnode.pos)
+        return minnode, minidx
+
+    def getForwardpoint(self, xnear, xrand):
+        l = self.disCal(xrand, xnear)
+        x = xnear.pos[0] + (xrand.pos[0] - xnear.pos[0]) / l
+        y = xnear.pos[1] + (xrand.pos[1] - xnear.pos[1]) / l
+        return Node((x, y))
+
+    def isTouchingEnd(self, xnew):
+        if self.disCal(xnew, self.end) < 1:
+            return True
+        return False
+
+    def makePath(self, cur):
+        path = []
+        while cur.pre != cur.pos:
+            path.append(cur.pos)
+            cur = self.T[cur.pre]
+            print(cur.pre)
+        path.append(cur.pos)
+        return path[::-1]
+
+    def findPath(self, n):
+        self.T.append(self.start)
+        path = []
+        for i in range(n):
+            xrand = self.getRandomPoint()
+            xnear, idx = self.getNearestPoint(xrand)
+            xnew = self.getForwardpoint(xnear, xrand)
+            if self.env.isNoCollision(xnear, xnew):
+                self.T.append(xnew)
+                xnew.setPre(idx)
+            if self.isTouchingEnd(xnew):
+                self.T.append(self.end)
+                self.T[-1].setPre(len(self.T)-2)
+                path = self.makePath(self.T[-1])
+                break
+        return path
+
+if __name__ == "__main__":
+    width = 100
+    height = 100
+    start = (1, 1)
+    end = (69, 76)
+    env = ContiEnvironment(width, height)
+    pathFinder = RRT(env, start, end)
+    path = pathFinder.findPath(5000)
+    env.showPath(path)
